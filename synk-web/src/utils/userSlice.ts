d9ccbd646b4e737d7d9c68812928fcfc04afc533
patiladitya77 <adpatil587@gmail.com>
2026-01-12 19:13:11 +0530

    Refactor: user slice

--- synk-web/src/utils/userSlice.ts
+++ synk-web/src/utils/userSlice.ts
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: null,
+  accessToken: null,
   isAuthenticated: false,
+  authLoading: true,
 };
 
 const userSlice = createSlice({
@@ -12,9 +14,16 @@ const userSlice = createSlice({
       state.user = action.payload;
       state.isAuthenticated = true;
     },
+    setAccessToken: (state, action) => {
+      state.accessToken = action.payload;
+    },
+    setAuthLoading: (state, action) => {
+      state.authLoading = action.payload;
+    },
     removeUser: () => initialState,
   },
 });
 
-export const { addUser, removeUser } = userSlice.actions;
+export const { addUser, setAccessToken, setAuthLoading, removeUser } =
+  userSlice.actions;
 export default userSlice.reducer;
