5a19fbe985fc3ada6dff6fe810779ba1dfffe7a5
patiladitya77 <adpatil587@gmail.com>
2026-02-20 17:06:52 +0530

    API: generate invite link

--- synk-server/src/controllers/boardController.ts
+++ synk-server/src/controllers/boardController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 import { generateSlug } from "../utils/generateSlug";
-
+import crypto from "crypto";
 export const createBoardController = async (req: Request, res: Response) => {
   try {
     const user = req.user;
@@ -105,6 +105,57 @@ export const checkName = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(400).json({
       message: "Failed to check board name",
+    });
+  }
+};
+
+export const generateInviteLinkController = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const { boardId } = req.params;
+    const user = req.user;
+    if (!user) {
+      return res.json({ message: "user does not exists" });
+    }
+    const board = await prisma.board.findUnique({ where: { id: boardId } });
+    if (!board || board.ownerId !== user.id) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+    if (
+      board?.inviteToken &&
+      board.isInviteEnabled &&
+      board.inviteTokenExpiresAt &&
+      board.inviteTokenExpiresAt > new Date()
+    ) {
+      const existingLink = `${process.env.ORIGIN_URL}invite/${board.inviteToken}`;
+
+      return res.json({
+        inviteLink: existingLink,
+        message: "Invite link already exists",
+      });
+    }
+
+    // Generate secure token
+    const token = crypto.randomBytes(32).toString("hex");
+
+    const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24); // 24 hours
+
+    await prisma.board.update({
+      where: { id: board.id },
+      data: {
+        inviteToken: token,
+        inviteTokenExpiresAt: expiresAt,
+        isInviteEnabled: true,
+      },
+    });
+
+    const inviteLink = `${process.env.ORIGIN_URL}invite/${token}`;
+    res.json({ message: "link created successfully", inviteLink, expiresAt });
+  } catch {
+    res.status(400).json({
+      message: "An error occured",
     });
   }
 };
--- synk-server/src/routes/boardRouter.ts
+++ synk-server/src/routes/boardRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   checkName,
   createBoardController,
+  generateInviteLinkController,
   getAllBoardsController,
 } from "../controllers/boardController";
 import { userAuth } from "../middleware/userAuth";
@@ -10,5 +11,10 @@ const boardRouter = express.Router();
 boardRouter.post("/create", userAuth, createBoardController);
 boardRouter.get("/getboards", userAuth, getAllBoardsController);
 boardRouter.get("/checkname", userAuth, checkName);
+boardRouter.post(
+  "/:boardId/generateinvitelink",
+  userAuth,
+  generateInviteLinkController,
+);
 
 export default boardRouter;
