9c36a7192b6dc44b8dfa9d328a326f6b915a3a1b
patiladitya77 <adpatil587@gmail.com>
2026-04-15 23:15:28 +0530

    updated shape registry

--- synk-web/src/canvas-engine/shapes/shapeRegistery.ts
+++ synk-web/src/canvas-engine/shapes/shapeRegistery.ts
@@ -1,8 +1,10 @@
 import { ShapeRenderer } from "../shapeRenderer";
+import { arrowRenderer } from "./ArrowRenderer";
 import { ovalRenderer } from "./OvalRenderer";
 import { rectRenderer } from "./rectangleRenderer";
 
 export const shapeRegistry: Record<string, ShapeRenderer> = {
   rect: rectRenderer,
   oval: ovalRenderer,
+  arrow: arrowRenderer,
 };
